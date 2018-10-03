9e8824d0afdeac23e71bdcea040fe9f22ac35e76
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-03 18:26:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :anagram
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(anaword)
+    anaword.select do |word|
+    word.chars.sort == @anagram.chars.sort
+    end
+  end
+
+
+
+end
