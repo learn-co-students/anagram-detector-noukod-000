8a5e86f2b2ea6fccf776c4d2028f2f61b248bb5d
Saint-Jean Germila <germila@noukod.com>
2018-10-10 14:19:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,11 @@
 class Anagram
 
-attr_accessor :word
+attr_accessor :name
 
 def initialize(word)
-  @word = word
+  @name = word
 end
-
+def match(array)
+ array.select {|x| x.split("").sort == @name.split("").sort}
+ end
 end# Your code goes here!
