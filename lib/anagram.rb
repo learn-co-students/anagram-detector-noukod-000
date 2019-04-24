9bbd1007a6c02f8051e6de6fcdb7dcfc0fac3961
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-24 18:55:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(word_array)
+    word_array.select{|el|@word.split("").sort == el.split("").sort }
+  end
+  
+end 
